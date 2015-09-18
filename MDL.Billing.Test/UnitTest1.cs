21086b5b8257a3b402eb9da6c683437e76a97c73
gbshukla <gbshukla@gmail.com>
2015-09-18 15:21:55 +0530

    Updated xUnit and related nuget packages

--- MDL.Billing.Test/UnitTest1.cs
+++ MDL.Billing.Test/UnitTest1.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MDL.Billing.Test
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
         public void TestMethod1()
         {
         }
