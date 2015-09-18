4f6b2a14c334e410d85ab9edf90f051d5e800199
gbshukla <gbshukla@gmail.com>
2015-09-18 15:05:15 +0530

    Initial commit

---
+++ MDL.Billing.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MDL.Billing.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
